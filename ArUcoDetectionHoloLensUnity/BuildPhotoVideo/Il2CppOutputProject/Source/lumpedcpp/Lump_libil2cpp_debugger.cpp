5267da2e63afc40063c6b3d50782527cdf9976ef
HCIE Lab Computer <mit.csail.hcie@gmail.com>
2022-05-13 17:53:09 -0400

    Remove BuildPhotoVideo folder.

--- ArUcoDetectionHoloLensUnity/BuildPhotoVideo/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\michi\UnityProjects\InfraTags\hololens\ArUcoDetectionHoloLens-Unity\ArUcoDetectionHoloLensUnity\BuildPhotoVideo\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
